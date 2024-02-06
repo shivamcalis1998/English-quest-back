fbc49041015871794827fafc49068e3711687817
unknown <shivamswami8899@gmail.com>
2024-02-06 18:12:13 +0530

    complete

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 const authRoute = require("./routes/authRoutes");
+const bookRoute = require("./routes/bookRoutes");
 app.use(cors());
 app.use(express.json());
 
 app.use("/", authRoute);
+app.use("/books", bookRoute);
 
 app.listen(port, async () => {
   console.log(`app is running on port ${port}`);
